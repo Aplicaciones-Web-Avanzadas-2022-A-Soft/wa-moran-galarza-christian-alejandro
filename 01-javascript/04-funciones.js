930295fe2d95cec65b04611fcbe27c2a5198fad2
Christian Morán <christian.moran@epn.edu.ec>
2022-05-29 20:02:58 -0500

    Más notaciones de funciones flecha

--- 01-javascript/04-funciones.js
+++ 01-javascript/04-funciones.js
@@ -29,3 +29,33 @@ funcionSinNombre3();
 const funcionFatArrow1 = () => {};
 var funcionFatArrow2 = () => {};
 let funcionFatArrow3 = () => {};
+[].forEach(()=>{})
+funcionFatArrow1();
+funcionFatArrow2();
+funcionFatArrow2();
+
+const funcionFatArrow4 = () => {};
+const funcionFatArrow5 = (x) => {
+    return x+1;
+};
+const funcionFatArrow6 = (x) => x+1; //FAT ARROW FUNCTIONS
+                                     // Una sola línea
+                                     // Omito Return
+                                     // Omito llaves
+const funcionFatArrow7 = x => x+1;   // SI SOLO TENEMOS UN PARÁMETRO
+                                     // Omito los paréntesis
+const funcionFatArrow8 = (x,y,z) => x+y+z;
+
+// ...=> Parámetros infinitos que llegan en un arreglo
+//       Solo se puede tener una de estas por función
+function sumarNumeros(...otrosNumeros){ //Parametros Inf [2,3,4,5,... ]
+    let total = 0;
+    otrosNumeros.forEach(
+        (valorActual) => {
+            total = total + valorActual;
+        }
+    );
+    return total;
+    // return otrosNumeros.reduce((a, v) => a+v, 0);
+}
+sumarNumeros(1,2,3,4,5,6,7,8,9,10,11,12,13)
