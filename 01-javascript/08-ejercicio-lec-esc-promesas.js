6bb04fe1ffe15097616a1001239fc91710658905
Christian Morán <christian.moran@epn.edu.ec>
2022-05-31 08:09:38 -0500

    Ejercicio completo 08-ejercicio-lec-esc-promesas.js

--- 01-javascript/08-ejercicio-lec-esc-promesas.js
+++ 01-javascript/08-ejercicio-lec-esc-promesas.js
@@ -42,7 +42,6 @@ function escribirArchivo(path, contenido){
             fs.writeFile(
                 path,
                 contenido,
-                //{flag: 'a+'},
                 (errorEscritura)=>{
                     if(errorEscritura){
                         reject('Error escribiendo archivo');
@@ -56,43 +55,53 @@ function escribirArchivo(path, contenido){
     return promesaEscritura;
 }
 
+//const path1= 'C:/Users/IntelHPi5/Documents/Software/Octavo Semestre/Aplicaciones Web Avanzadas/wa-moran-galarza-christian-alejandro/01-javascript/06-ejemplo.txt';
+//const path1='C:\\Users\\SIGMA\\Documents\\wa-moran-galarza-christian-alejandro\\01-javascript\\06-ejemplo.txt';
+//const path2= 'C:/Users/IntelHPi5/Documents/Software/Octavo Semestre/Aplicaciones Web Avanzadas/wa-moran-galarza-christian-alejandro/01-javascript/01-variables.js';
+//const path2='C:\\Users\\SIGMA\\Documents\\wa-moran-galarza-christian-alejandro\\01-javascript\\01-variables.js';
 function crearNuevoArchivo(path1, path2, path3){
-    const promesaCrear = new Promise(
-        (
-            resolve,
-            reject
-        )=>{
-            leerArchivo(path1)
-                .then(
-                    (contenido1)=>{
-                        return escribirArchivo(path3,contenido1);
-                    }
-                )
-                .then(
-                    (mensaje)=>{
-                        console.log(mensaje);
-                    }
-                );
-            leerArchivo(path2)
-                .then(
-                    (contenido2)=>{
-                        return escribirArchivo(path3,contenido2);
-                    }
-                )
-                .then(
-                    (mensaje)=>{
-                        console.log(mensaje);
-                    }
-                );
-            resolve('Archivo creado con éxito');
-        }
-    )
-    return promesaCrear;
+    let contenido = '';
+    leerArchivo(path1)
+        .then(
+               (contenido1)=> {
+                   contenido = contenido + contenido1;
+                   return leerArchivo(path2);
+               }
+        )
+        .then(
+              (contenido2)=>{
+                  contenido = contenido + contenido2;
+                  return escribirArchivo(path3, contenido);
+              }
+        )
+        .then(
+              (mensaje)=>{
+                  console.log(mensaje);
+              }
+        )
+        .catch(
+            (error) => {
+                console.error(error);
+            }
+        )
 }
 
-crearNuevoArchivo('./01-variables.js','02-arreglos.js','./08-nuevo-archivo.txt')
-    .then(
-        (respuesta)=>{
-            console.log(respuesta);
-        }
-    );
+crearNuevoArchivo('C:\\Users\\SIGMA\\Documents\\wa-moran-galarza-christian-alejandro\\01-javascript\\06-ejemplo.txt',
+    'C:\\Users\\SIGMA\\Documents\\wa-moran-galarza-christian-alejandro\\01-javascript\\01-variables.js',
+    './08-nuevo-archivo.txt');
+// ASYNC AWAIT
+// REGLAS:
+// 1) Estar dentro de una funcion (nombrada o anonima)
+// 2) AGREGAR la palabra 'async' antes de la declaracion
+//    de la funcion
+// 3) AGREGAR la palabra 'await' antes de la declaracion
+//    de una promesa
+async function ejecutarPromesasAsyncAwait(pathUno, pathDos, pathTres) {
+    try {
+        const primerContenido = await promesaLectura(pathUno);
+        const segundoContenido = await promesaLectura(pathUno);
+        await promesaEscritura(pathTres, primerContenido + segundoContenido);
+    } catch (error) {
+        console.error(error);
+    }
+}
