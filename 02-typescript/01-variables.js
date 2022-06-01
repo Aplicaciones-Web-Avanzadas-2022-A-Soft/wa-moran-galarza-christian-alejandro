d0a911bd697260f776bfd64e1475c4400196522b
Christian Morán <christian.moran@epn.edu.ec>
2022-06-01 08:36:56 -0500

    Transpilación de TS a JS

--- 02-typescript/01-variables.js
+++ 02-typescript/01-variables.js
@@ -1,26 +1,26 @@
 // 01-variables.ts
-var nombre = 'Chris'; // Primitivo
-var nombre2 = 'Chris2'; // Clase String
+let nombre = 'Chris'; // Primitivo
+let nombre2 = 'Chris2'; // Clase String
 // nombre = 1;
 nombre = 'Alejandro';
-var edad = 24;
-var casado = false;
-var fecha = new Date();
-var sueldo;
+let edad = 24;
+let casado = false;
+let fecha = new Date();
+let sueldo;
 sueldo = 12.4;
 // Duck Typing
-var apellido = 'Moran'; // string
+let apellido = 'Moran'; // string
 apellido = 'Chris'; // igual a otros string
 apellido.toUpperCase(); // métodos string
 // apellido = 1; // Error, no es un string
-var marihuana = 2;
+let marihuana = 2;
 marihuana = '2';
 marihuana = true;
-marihuana = function () { return '2'; };
-var edadMultiple = '2'; // o 2 o new Date();
+marihuana = () => '2';
+let edadMultiple = '2'; // o 2 o new Date();
 edadMultiple = '2';
 edadMultiple = 2222;
-var numeroUnico = 1; // para igualar a otros se castea
+let numeroUnico = 1; // para igualar a otros se castea
 numeroUnico = edadMultiple;
 edadMultiple = 'dos';
 edadMultiple = new Date();
