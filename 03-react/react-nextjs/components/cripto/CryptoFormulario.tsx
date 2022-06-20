40c314be5324c83d78405738239d97ecf4951cc2
Christian Morán <christian.moran@epn.edu.ec>
2022-06-20 09:00:01 -0500

    Hook useEffect

--- 03-react/react-nextjs/components/cripto/CryptoFormulario.tsx
+++ 03-react/react-nextjs/components/cripto/CryptoFormulario.tsx
@@ -1,24 +1,60 @@
-import {MONEDAS} from "../constantes/monedas"
+import {MONEDAS, MonedasInterface} from "../constantes/monedas"
 import useSelectMoneda from "../hooks/useSelectMoneda";
+import {useEffect, useState} from "react";
 
 export const CryptoFormulario = () => {
-    const[SelectMonedaComponente] = useSelectMoneda()
-    const generarSelectMonedas = () => {
-        return MONEDAS.map((moneda) =>(
-                <option id={moneda.id} value={moneda.id}> {moneda.nombre}</option>
-            )
-        )
-    }
-
+    const [monedasArreglo, setMonedasArreglo] = useState(
+        // MONEDAS.map((a)=>a),
+        // Object.assign([], MONEDAS)
+        [...MONEDAS]
+    );
+    const [criptoMonedasArreglo, setCriptoMonedasArreglo] = useState([]);
+    // Definir selects
+    const[SelectMonedaComponente] = useSelectMoneda('Seleccionar moneda', monedasArreglo)
+    const[SelectCriptoMonedaComponente] = useSelectMoneda('Seleccionar criptomoneda', criptoMonedasArreglo)
+    // Ayuda a reaccionar a cambios, debido a que alguna variable escucha cambios
+    // Inicializar variables dentro del componente
+    useEffect(
+        () => {
+            // eventos cuando cambie variable
+            // setCriptoMonedasArreglo([{id: 'Bitcoin',nombre: 'Bitcoin'}])
+            const consultarAPI = async () => {
+                const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+                const respuesta = await fetch(url);
+                const dataPlana = await respuesta.json();
+                const arregloCriptos = dataPlana.Data.map(
+                    (criptoMoneda)=>{
+                        const criptoMonedaLocal:MonedasInterface = {
+                            id: criptoMoneda.CoinInfo.Name,
+                            nombre: criptoMoneda.CoinInfo.FullName
+                        };
+                        return criptoMonedaLocal;
+                    }
+                );
+                setCriptoMonedasArreglo(arregloCriptos)
+            }
+            consultarAPI();
+        },
+        [
+            // Arreglo de variables a escuchar
+        ]
+    )
+    // const generarSelectMonedas = () => {
+    //     return MONEDAS.map((moneda) =>(
+    //             <option id={moneda.id} value={moneda.id}> {moneda.nombre}</option>
+    //         )
+    //     )
+    // }
     return(
         <>
             <form>
-                <label className="form-label" htmlFor="moneda">Moneda </label>
-                <select className="form-select" name="moneda" id="moneda">
-                    <option value="">Seleccione opción</option>
-                    {generarSelectMonedas()}
-                </select>
+                {/*<label className="form-label" htmlFor="moneda">Moneda </label>*/}
+                {/*<select className="form-select" name="moneda" id="moneda">*/}
+                {/*    <option value="">Seleccione opción</option>*/}
+                {/*    {generarSelectMonedas()}*/}
+                {/*</select>*/}
                 <SelectMonedaComponente/>
+                <SelectCriptoMonedaComponente/>
             </form>
         </>
     )
--- 03-react/react-nextjs/components/hooks/useSelectMoneda.tsx
+++ 03-react/react-nextjs/components/hooks/useSelectMoneda.tsx
@@ -1,33 +1,41 @@
 import {useState} from "react";
+import {MonedasInterface} from "../constantes/monedas";
 
-const useSelectMoneda = () =>{
+const useSelectMoneda = (label:string, opciones: MonedasInterface[]) =>{
     // Usamos Hooks React (userState)
     // const [state, setState] = useState('VALOR')
     const [valorEjemplo, setValorEjemplo] = useState('Christian Moran')
+    const generarSelectMonedas = () => {
+        return opciones.map((moneda) =>(
+                <option id={moneda.id} value={moneda.id}> {moneda.nombre}</option>
+            )
+        )
+    }
     const SelectMonedas = () => (
         <>
-            <label className="form-label" htmlFor="moneda">Moneda </label>
-            <select className="form-select" name="moneda" id="moneda">
+            <label className="form-label" htmlFor={label}>{label}</label>
+            <select className="form-select" name={label} id={label}>
                 <option value="">Seleccione opción</option>
+                {generarSelectMonedas()}
             </select>
-            <label className="form-label" htmlFor="moneda">Moneda </label>
-            <label className="form-label" htmlFor="ejemplo">Ejemplo </label>
-            <p>El valor es: {valorEjemplo}</p>
-            <input className="form-control"
-                   value={valorEjemplo}
-                   id="ejemplo"
-                   onChange={(e)=>{
-                       e.preventDefault();
-                       setValorEjemplo(e.target.value)
-                   }}
-                   type="text"/>
-            <button onClick={
-                (event)=>{
-                    event.preventDefault();
-                    setValorEjemplo('Christian '+new Date().getTime());
-                }
-            }
-            >Cambiar ejemplo</button>
+            {/*<label className="form-label" htmlFor="moneda">Moneda </label>*/}
+            {/*<label className="form-label" htmlFor="ejemplo">Ejemplo </label>*/}
+            {/*<p>El valor es: {valorEjemplo}</p>*/}
+            {/*<input className="form-control"*/}
+            {/*       value={valorEjemplo}*/}
+            {/*       id="ejemplo"*/}
+            {/*       onChange={(e)=>{*/}
+            {/*           e.preventDefault();*/}
+            {/*           setValorEjemplo(e.target.value)*/}
+            {/*       }}*/}
+            {/*       type="text"/>*/}
+            {/*<button onClick={*/}
+            {/*    (event)=>{*/}
+            {/*        event.preventDefault();*/}
+            {/*        setValorEjemplo('Christian '+new Date().getTime());*/}
+            {/*    }*/}
+            {/*}*/}
+            {/*>Cambiar ejemplo</button>*/}
         </>
     )
     return [SelectMonedas]
