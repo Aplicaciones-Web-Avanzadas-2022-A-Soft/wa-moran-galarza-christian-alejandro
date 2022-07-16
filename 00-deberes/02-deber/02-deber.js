a88a5a91becfbda05829061118f590fb76709173
Christian Morán <christian.moran@epn.edu.ec>
2022-07-16 18:30:10 -0500

    Deber 2 final

--- 00-deberes/02-deber/02-deber.js
+++ 00-deberes/02-deber/02-deber.js
@@ -15,7 +15,7 @@ function leerArchivo(path){
                 'utf-8',
                 (error, contenido) => {
                     if(error){
-                        reject('Error leyendo archivo');
+                        reject('Error leyendo archivo.');
                     }else{
                         resolve(contenido);
                     }
@@ -36,9 +36,9 @@ function escribirArchivo(path, contenido){
                 contenido,
                 (errorEscritura)=>{
                     if(errorEscritura){
-                        reject('Error escribiendo archivo');
+                        reject('Error escribiendo archivo.');
                     }else{
-                        resolve('Archivo escrito');
+                        resolve('Archivo escrito.');
                     }
                 }
             );
@@ -50,7 +50,7 @@ async function guardarDatos(){
     try{
         await escribirArchivo(path, JSON.stringify(arregloDatos));
     }catch (e) {
-        console.log('Error en la escritura de los datos', e);
+        console.log('Error en la escritura de los datos.', e);
     }
 }
 
@@ -83,16 +83,16 @@ async function recibirId(nombreEntidad){
     }
 }
 
-let manager = {
-    id: 0,
-    nombre: '',
-    apellido: '',
-    direccion: '',
-    fechaNacimiento: '',
-    disquera: '',
-    sueldo: 0,
-    artistas: []
-}
+// let manager = {
+//     id: 0,
+//     nombre: '',
+//     apellido: '',
+//     direccion: '',
+//     fechaNacimiento: '',
+//     disquera: '',
+//     sueldo: 0,
+//     artistas: []
+// }
 
 async function crearManager(){
     try{
@@ -115,7 +115,7 @@ async function crearManager(){
             {
                 type: 'input',
                 name: 'direccion',
-                message: 'Ingresa la direccion del manager:'
+                message: 'Ingresa la dirección del manager:'
             },
             {
                 type: 'input',
@@ -137,14 +137,16 @@ async function crearManager(){
             elemento => elemento.id === respuestas.id
         )
         if(indice !== -1){
-            console.log('Ya existe un manager con ese identificador.')
+            console.log('\n\nYa existe un manager con ese identificador.\n')
         }else{
             respuestas.artistas = [];
             respuestas.tipo = 'Manager';
             arregloDatos.push(respuestas);
+            console.log('\n\nManager creado con éxito.\n')
+            await leerManager();
         }
     }catch (e) {
-        console.log('Error al crear un Manager', e);
+        console.log('\n\nError al crear un Manager.', e);
     }
 }
 
@@ -156,10 +158,10 @@ const leerManager = () => {
             }
         );
     if(arregloManagers.length !== 0){
-        console.log('\t\tManagers');
+        console.log('\n\n\t\tListado de Managers');
         console.log(arregloManagers);
     }else{
-        console.log('No existen managers');
+        console.log('\n\nNo existen managers.\n');
     }
 }
 
@@ -184,7 +186,7 @@ async function actualizarManager(){
                 {
                     type: 'input',
                     name: 'direccion',
-                    message: 'Ingresa la direccion del manager:'
+                    message: 'Ingresa la dirección del manager:'
                 },
                 {
                     type: 'input',
@@ -208,11 +210,13 @@ async function actualizarManager(){
             arregloDatos[indice].fechaNacimiento = respuestas.fechaNacimiento;
             arregloDatos[indice].disquera = respuestas.disquera;
             arregloDatos[indice].sueldo = respuestas.sueldo;
+            console.log('\n\nManager actualizado con éxito.\n')
+            await leerManager();
         }catch (e) {
-            console.log('No se pudo actualizar al manager',e)
+            console.log('\n\nNo se pudo actualizar al manager.',e)
         }
     }else{
-        console.log('No existe un manager con ese identificador.')
+        console.log('\n\nNo existe un manager con ese identificador.\n')
     }
 }
 
@@ -232,26 +236,182 @@ async function borrarManager(){
             )
             if(respuesta.confirmacion){
                 arregloDatos.splice(indice, 1);
-                console.log('Manager eliminado');
+                console.log('\n\nManager eliminado con éxito.\n');
+                await leerManager();
             }else{
-                console.log('Se cancelo la eliminacion del manager');
+                console.log('\n\nSe canceló la eliminación del manager.\n');
             }
         }catch (e) {
-            console.log('No se pudo eliminar al manager',e)
+            console.log('\n\nNo se pudo eliminar al manager.',e)
         }
     }else{
-        console.log('No existe un manager con ese identificador.')
+        console.log('\n\nNo existe un manager con ese identificador.\n')
     }
 }
 
-let artista = {
-    id: 0,
-    nombre: '',
-    apellido: '',
-    direccion: '',
-    fechaNacimiento: '',
-    disponibilidad: false,
-    discos: []
+// let artista = {
+//     id: 0,
+//     nombre: '',
+//     apellido: '',
+//     direccion: '',
+//     fechaNacimiento: '',
+//     disponibilidad: false,
+//     discos: []
+// }
+
+async function crearArtista(){
+    try{
+        const respuestas = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Ingresa el id del artista:'
+            },
+            {
+                type: 'input',
+                name: 'nombre',
+                message: 'Ingresa el nombre del artista:'
+            },
+            {
+                type: 'input',
+                name: 'apellido',
+                message: 'Ingresa el apellido del artista:'
+            },
+            {
+                type: 'input',
+                name: 'direccion',
+                message: 'Ingresa la dirección del artista:'
+            },
+            {
+                type: 'input',
+                name: 'fechaNacimiento',
+                message: 'Ingresa la fecha de nacimiento del artista:'
+            },
+            {
+                type: 'input',
+                name: 'disponibilidad',
+                message: 'Ingresa la disponibilidad del artista:'
+            },
+            {
+                type: 'number',
+                name: 'numeroDiscos',
+                message: 'Ingresa el numero de discos del artista:'
+            },
+        ])
+        const indice = arregloDatos.findIndex(
+            elemento => elemento.id === respuestas.id
+        )
+        if(indice !== -1){
+            console.log('\n\nYa existe un artista con ese identificador.\n')
+        }else{
+            respuestas.discos = [];
+            respuestas.tipo = 'Artista';
+            arregloDatos.push(respuestas);
+            console.log('\n\nArtista creado con éxito.\n')
+            await leerArtista();
+        }
+    }catch (e) {
+        console.log('\n\nError al crear un Artista.', e);
+    }
+}
+
+const leerArtista = () => {
+    const arregloArtistas = arregloDatos
+        .filter(
+            (valorActual, indiceActual, arregloCompleto)=>{
+                return valorActual.tipo === 'Artista';
+            }
+        );
+    if(arregloArtistas.length !== 0){
+        console.log('\n\n\t\tListado de Artistas');
+        console.log(arregloArtistas);
+    }else{
+        console.log('\n\nNo existen artistas.\n');
+    }
+}
+
+async function actualizarArtista(){
+    let idArtista = await recibirId(' artista ');
+    const indice = arregloDatos.findIndex(
+        elemento => elemento.id === idArtista
+    )
+    if(indice !== -1){
+        try{
+            const respuestas = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'nombre',
+                    message: 'Ingresa el nombre del artista:'
+                },
+                {
+                    type: 'input',
+                    name: 'apellido',
+                    message: 'Ingresa el apellido del artista:'
+                },
+                {
+                    type: 'input',
+                    name: 'direccion',
+                    message: 'Ingresa la dirección del artista:'
+                },
+                {
+                    type: 'input',
+                    name: 'fechaNacimiento',
+                    message: 'Ingresa la fecha de nacimiento del artista:'
+                },
+                {
+                    type: 'input',
+                    name: 'disponibilidad',
+                    message: 'Ingresa la disponibilidad del artista:'
+                },
+                {
+                    type: 'number',
+                    name: 'numeroDiscos',
+                    message: 'Ingresa el numero de discos del artista:'
+                },
+            ])
+            arregloDatos[indice].nombre = respuestas.nombre;
+            arregloDatos[indice].apellido = respuestas.apellido;
+            arregloDatos[indice].direccion = respuestas.direccion;
+            arregloDatos[indice].fechaNacimiento = respuestas.fechaNacimiento;
+            arregloDatos[indice].disponibilidad = respuestas.disponibilidad;
+            arregloDatos[indice].numeroDiscos = respuestas.numeroDiscos;
+            console.log('\n\nArtista actualizado con éxito.\n')
+            await leerArtista();
+        }catch (e) {
+            console.log('\n\nNo se pudo actualizar al artista.',e)
+        }
+    }else{
+        console.log('\n\nNo existe un manager con ese identificador.\n')
+    }
+}
+
+async function borrarArtista(){
+    let idManager = await recibirId(' artista ');
+    const indice = arregloDatos.findIndex(
+        elemento => elemento.id === idManager
+    )
+    if(indice !== -1){
+        try{
+            const respuesta = await inquirer.prompt(
+                {
+                    type: "confirm",
+                    name: "confirmacion",
+                    message: "¿Seguro desea eliminar al artista?"
+                }
+            )
+            if(respuesta.confirmacion){
+                arregloDatos.splice(indice, 1);
+                console.log('\n\nArtista eliminado con éxito.\n');
+                await leerArtista();
+            }else{
+                console.log('\n\nSe canceló la eliminación del artista.\n');
+            }
+        }catch (e) {
+            console.log('\n\nNo se pudo eliminar al artista.',e)
+        }
+    }else{
+        console.log('\n\nNo existe un artista con ese identificador.\n')
+    }
 }
 
 async function menu(){
@@ -273,7 +433,7 @@ async function menu(){
                 {
                     type: 'number',
                     name: 'opcion',
-                    messaje: 'Escoja una opcion: '
+                    messaje: 'Escoja una opción: '
                 }
             )
             opcion = respuesta.opcion;
@@ -293,12 +453,24 @@ async function menu(){
                 case 4:
                     await borrarManager();
                     break;
+                case 5:
+                    await crearArtista();
+                    break;
+                case 6:
+                    leerArtista();
+                    break;
+                case 7:
+                    await actualizarArtista();
+                    break;
+                case 8:
+                    await borrarArtista();
+                    break;
                 default:
-                    console.log('No es una orden adecuada')
+                    console.log('No es una orden del menú.')
                     break;
             }
         }catch (e) {
-            console.log('No se puede acceder al menu', e);
+            console.log('No se puede acceder al menú.', e);
         }
     }
 }
