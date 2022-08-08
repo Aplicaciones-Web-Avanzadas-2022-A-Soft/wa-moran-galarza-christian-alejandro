23fad59d296f8189fb01aee2bfbac28ed58278db
Christian Morán <christian.moran@epn.edu.ec>
2022-08-08 08:55:11 -0500

    Hasta creación de las entidades

--- 04-websockets-nestjs/ws-server/src/app.module.ts
+++ 04-websockets-nestjs/ws-server/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {EventosModule} from "./eventos/eventos.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
+// Modulo principal
 @Module({
-  imports: [EventosModule],
+  imports: [
+      EventosModule,
+      TypeOrmModule.forRoot({
+        name: 'default', // nombre de la conexion
+        type: 'sqlite',
+        database: './bdd/bdd.sqlite',
+        entities: [UsuarioEntity], // entidades del aplicativo completo
+        synchronize: true,
+        dropSchema: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
